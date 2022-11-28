82c6d4370fd186239eae42d1074e102ceee7ba16
Dozer8383 <119268610+dozer8383@users.noreply.github.com>
2022-11-27 19:20:31 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+class Utilities {
+    constructor (runtime, extensionId) {
+        this.runtime = runtime;
+    }
+
+    getInfo () {
+        return {
+            "id": 'utilities',
+            "name": 'Utilities',
+            "blocks": [
+                {
+                    "opcode": 'block',
+                    "blockType": "reporter",
+                    "text": "Block",
+                    "arguments": {
+                        "INPUT": {
+                            "type": "string",
+                            "menu": "menu",
+                            "defaultValue": "Hi"
+                        }
+                    }
+                }
+            ],
+            "menus": {
+                "menu": {
+                    "items": ["Hi", "Bye"]
+                },
+            },
+        };
+    };
+    
+    // Code for blocks go here
+
+    block({INPUT}){
+        return INPUT
+    };
+};
+
+(function() {
+    var extensionClass = Utilities;
+    if (typeof window === "undefined" || !window.vm) {
+        Scratch.extensions.register(new extensionClass());
+    } else {
+        var extensionInstance = new extensionClass(window.vm.extensionManager.runtime);
+        var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance);
+        window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName);
+    };
+})()
