28a7c489517fb2aa011d3d10e71eebb785f57cf6
Dozer8383 <119268610+dozer8383@users.noreply.github.com>
2022-11-29 20:43:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -109,3 +109,114 @@ class Utilities {
         window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName);
     };
 })()
+class JavaScript {
+    constructor (runtime, extensionId) {
+        this.runtime = runtime;
+    }
+
+    getInfo () {
+        return {
+            "id": 'javascript',
+            "name": 'JavaScript',
+            "color1": '#ffff00',
+            "color2": '#ecec00',
+            "blocks": [
+                {
+                    "opcode": 'average',
+                    "blockType": "reporter",
+                    "text": "average of [INPUT]",
+                    "arguments": {
+                        "INPUT": {
+                            "type": "string",
+                            "defaultValue": "10 20"
+                        }
+                    }
+                },
+                {
+                    "opcode": 'randomdigits',
+                    "blockType": "reporter",
+                    "text": "generate [INPUT] random digits",
+                    "arguments": {
+                        "INPUT": {
+                            "type": "number",
+                            "defaultValue": 10
+                        }
+                    }
+                },
+                {
+                    "opcode": 'gettimezone',
+                    "blockType": "reporter",
+                    "text": "get current timezone offset",
+                    "arguments": {
+                    }
+                },
+                {
+                    "opcode": 'slice',
+                    "blockType": "reporter",
+                    "text": "characters [BEGINNING] to [END] in text [TEXT]",
+                    "arguments": {
+                        "BEGINNING": {
+                            "type": "number",
+                            "defaultValue": 1
+                        },
+                        "END": {
+                            "type": "number",
+                            "defaultValue": 5
+                        },
+                        "TEXT": {
+                            "type": "string",
+                            "defaultValue": "hello there"
+                        }
+                    }
+                }
+            ],
+//             "menus": {
+//                 "timezones": {
+//                     "items": []
+//                 },
+//             },
+        };
+    };
+    
+    // Code for blocks go here
+
+    average({INPUT}){
+        var inputs = INPUT.split(" ")
+        var output = 0
+        inputs.forEach(input => {
+            output += parseInt(input)
+        })
+        output = output / inputs.length
+        return output
+    };
+
+    randomdigits({INPUT}){
+        var output = ""
+        for (let i = 0; i < INPUT; i++) {
+            output += `${Math.round(Math.random() * 9)}`
+        }
+        return output
+    };
+    
+    gettimezone(){
+        var date = new Date()
+        var offset = date.getTimezoneOffset()
+        return offset   
+    }
+
+    slice({BEGINNING, END, TEXT}){
+        var texttoslice = TEXT.toString()
+        return texttoslice.slice(BEGINNING - 1, END)
+    };
+};
+
+(function() {
+    var extensionClass = JavaScript;
+    if (typeof window === "undefined" || !window.vm) {
+        Scratch.extensions.register(new extensionClass());
+    } else {
+        var extensionInstance = new extensionClass(window.vm.extensionManager.runtime);
+        var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance);
+        window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName);
+    };
+})()
